850a9fdc6da0191172e30fca6d6bf1bf7de9eb4c
xxxxxxxb <96876879+xxxxxxxb@users.noreply.github.com>
2022-04-09 22:37:10 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -3,8 +3,8 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'img/2233jk.jpg') {
-      myImage.setAttribute('src', 'img/2233jk.jpg');
-    } else {
       myImage.setAttribute('src', 'img/22d1098286b9f162041cd9c67c5377f0.jpg');
+    } else {
+      myImage.setAttribute('src', 'img/2233jk.jpg');
     }
 }
