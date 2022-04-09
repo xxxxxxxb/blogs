16fb6d6c02e13a6fbfbd8a66b1faae3ef10f098a
xxxxxxxb <96876879+xxxxxxxb@users.noreply.github.com>
2022-04-09 22:22:01 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -2,9 +2,9 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute('src', 'images/firefox2.png');
+    if(mySrc === 'img/2233jk.jpg') {
+      myImage.setAttribute('src', 'img/2233jk.jpg');
     } else {
-      myImage.setAttribute('src', 'images/firefox-icon.png');
+      myImage.setAttribute('src', 'img/22d1098286b9f162041cd9c67c5377f0.jpg');
     }
 }
