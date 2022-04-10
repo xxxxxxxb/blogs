a4801d78f2555cf922f92be90b7881ff898fc94f
xxxxxxxb <96876879+xxxxxxxb@users.noreply.github.com>
2022-04-10 08:48:26 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -2,9 +2,9 @@ let myImage = document.querySelector(#img);
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'img/2233jk.jpg') {
-      myImage.setAttribute('src', 'img/22d1098286b9f162041cd9c67c5377f0.jpg');
+    if(mySrc === 'main/img/2233jk.jpg') {
+      myImage.setAttribute('src', 'main/img/22d1098286b9f162041cd9c67c5377f0.jpg');
     } else {
-      myImage.setAttribute('src', 'img/2233jk.jpg');
+      myImage.setAttribute('src', 'main/img/2233jk.jpg');
     }
 }
