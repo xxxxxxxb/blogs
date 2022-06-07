38cebc9e9e8dceb67c79825eb74c578887b260b0
xxxxxxxb <96876879+xxxxxxxb@users.noreply.github.com>
2022-06-07 22:25:44 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -3,8 +3,8 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'main/img/2233jk.jpg') {
-      myImage.setAttribute('src', './main/img/22d1098286b9f162041cd9c67c5377f0.jpg');
+      myImage.setAttribute('src', 'main/img/v2-89aa40908b00c7d23fd265e897fe2989_r.jpg');
     } else {
-      myImage.setAttribute('src', './main/img/2233jk.jpg');
+      myImage.setAttribute('src', 'main/img/2233jk.jpg');
     }
 }
