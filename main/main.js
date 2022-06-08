761e3b31755c9d7b309fcc07d4a3f129e723f269
xxxxxxxb <96876879+xxxxxxxb@users.noreply.github.com>
2022-06-08 18:47:41 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,13 +1,13 @@
 let myImage = document.querySelector('img');
 myImage.onclick = function() {
     mySrc = myImage.getAttribute('src');
-    if(mySrc === '2233jk.jpg'){
-        myImage.setAttribute('src','v2-89aa40908b00c7d23fd265e897fe2989_r.jpg');
+    if(mySrc === 'main/img/2233jk.jpg'){
+        myImage.setAttribute('src','main/img/v2-89aa40908b00c7d23fd265e897fe2989_r.jpg');
     }
-    else if(mySrc === 'v2-89aa40908b00c7d23fd265e897fe2989_r.jpg'){
+    else if(mySrc === 'main/img/v2-89aa40908b00c7d23fd265e897fe2989_r.jpg'){
         myImage.setAttribute('src','02789FA6-ABFD-47FB-9630-552DE0575855.jpeg');
     }
     else{
-         myImage.setAttribute('src','2233jk.jpg');
+         myImage.setAttribute('src','main/img/2233jk.jpg');
     }
 }
