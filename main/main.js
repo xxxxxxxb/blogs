e2aa86b72a0707c74863a0f751fbd29dfdd901ab
xxxxxxxb <96876879+xxxxxxxb@users.noreply.github.com>
2022-06-08 20:52:27 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -5,7 +5,7 @@ myImage.onclick = function() {
         myImage.setAttribute('src','main/img/v2-89aa40908b00c7d23fd265e897fe2989_r.jpg');
     }
     else if(mySrc === 'main/img/v2-89aa40908b00c7d23fd265e897fe2989_r.jpg'){
-        myImage.setAttribute('src','02789FA6-ABFD-47FB-9630-552DE0575855.jpeg');
+        myImage.setAttribute('src','main/img/02789FA6-ABFD-47FB-9630-552DE0575855.jpeg');
     }
     else{
          myImage.setAttribute('src','main/img/2233jk.jpg');
