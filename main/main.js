90fad51c4c2cd0a2697b44546f2b9f628f76f946
xxxxxxxb <96876879+xxxxxxxb@users.noreply.github.com>
2022-06-08 18:44:21 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -2,12 +2,12 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
     mySrc = myImage.getAttribute('src');
     if(mySrc === '2233jk.jpg'){
-        myImage.setAttribute('src',);
+        myImage.setAttribute('src','v2-89aa40908b00c7d23fd265e897fe2989_r.jpg');
     }
-    else if(mySrc === ' '){
-        myImage.setAttribute('src',);
+    else if(mySrc === 'v2-89aa40908b00c7d23fd265e897fe2989_r.jpg'){
+        myImage.setAttribute('src','02789FA6-ABFD-47FB-9630-552DE0575855.jpeg');
     }
     else{
-         myImage.setAttribute('src',);
+         myImage.setAttribute('src','2233jk.jpg');
     }
 }
