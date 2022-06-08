be9b714ac44f630c9819bc1750e8ed7fa70547a3
xxxxxxxb <96876879+xxxxxxxb@users.noreply.github.com>
2022-06-08 18:36:53 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,10 +1,34 @@
 let myImage = document.querySelector('img');
-
+class HashMap {
+constructor(initialCapacity = 4) {
+this.buckets = new Array(initialCapacity);
+}
+set(key, value) {
+const index = this.getIndex(key);
+this.buckets[index] = value;
+}
+get(key) {
+const index = this.getIndex(key);
+return this.buckets[index];
+}
+hash(key) {
+return key.toString().length;
+}
+getIndex(key) {
+const indexHash = this.hash(key);
+const index = indexHash % this.buckets.length;
+return index;
+}
+var photos = HashMap(114514);
+photos.set(0,'main/img/2233jk.jpg');
+photos.set(1,'main/img/v2-89aa40908b00c7d23fd265e897fe2989_r.jpg')
+photos.set(2,'')
+var cnt = 0;
 myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'main/img/2233jk.jpg') {
-      myImage.setAttribute('src', 'main/img/v2-89aa40908b00c7d23fd265e897fe2989_r.jpg');
-    } else {
-      myImage.setAttribute('src', 'main/img/2233jk.jpg');
+    if(cnt === 4){
+        cnt = 0;
     }
+    let mySrc = myImage.getAttribute('src');
+    myImage.setAttribute('src', photos.get(cnt + 1);
+    cnt = cnt + 1;
 }
